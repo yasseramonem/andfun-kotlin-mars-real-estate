faa8adc9dab06cfec73ee9bb9692aac9c249ca18
Yasser Abdelmonem <yasseramonem@gmail.com>
2021-07-20 01:22:30 +0200

    Using a viewModelScope Coroutine with a try & catch instead of Callback Object

--- app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
+++ app/src/main/java/com/example/android/marsrealestate/overview/OverviewViewModel.kt
@@ -20,11 +20,9 @@ package com.example.android.marsrealestate.overview
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.android.marsrealestate.network.MarsAPI
-import com.example.android.marsrealestate.network.MarsProperty
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.launch
 
 /**
  * The [ViewModel] that is attached to the [OverviewFragment].
@@ -49,17 +47,20 @@ class OverviewViewModel : ViewModel() {
      * Sets the value of the status LiveData to the Mars API status.
      */
     private fun getMarsRealEstateProperties() {
-        MarsAPI.retrofitService.getProperties().enqueue(object: Callback<List<MarsProperty>> {
-            override fun onResponse(call: Call<List<MarsProperty>>, response: Response<List<MarsProperty>>) {
 
-                _response.value = "Success ${response.body()?.size} Mars Properties retrieved "
+        viewModelScope.launch {
+            try {
+                var listResult = MarsAPI.retrofitService.getProperties()
+                _response.value = "Success ${listResult.size} Mars Properties retrieved "
+
+            } catch (e: Exception){
+                _response.value = "Failure ${e.message}"
 
             }
 
-            override fun onFailure(call: Call<List<MarsProperty>>, t: Throwable) {
-                _response.value = "Failure" + t.message
-            }
 
-        })
+        }
     }
+
 }
+
